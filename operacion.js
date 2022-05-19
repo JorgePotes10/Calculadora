a859522fb10bcbe9213e33d8359fa933fd0e6ffa
Your Name <you@example.com>
2022-05-19 16:27:13 -0500

    subida

---
+++ operacion.js
@@ -0,0 +1,12 @@
+function sumar(){
+
+   let numero1  =parseInt($("#inputNumero1").val()); 
+   let numero2 = parseInt($("#inputNumero2").val()); 
+
+   operacion = (numero1+numero2);
+   
+   
+   alert(operacion);
+
+}
+
